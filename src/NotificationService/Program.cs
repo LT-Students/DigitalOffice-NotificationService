3d668c7ec7e3c561fe3bc558c2273608e5fbae48
asyagraf <67205781+asyagraf@users.noreply.github.com>
2021-12-27 16:55:57 +0300

    fix

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -11,8 +11,8 @@ namespace LT.DigitalOffice.NotificationService
     public static void Main(string[] args)
     {
       var configuration = new ConfigurationBuilder()
-          .AddJsonFile("appsettings.json")
-          .Build();
+        .AddJsonFile("appsettings.json")
+        .Build();
 
       string seqServerUrl = Environment.GetEnvironmentVariable("seqServerUrl");
       if (string.IsNullOrEmpty(seqServerUrl))
@@ -27,12 +27,12 @@ namespace LT.DigitalOffice.NotificationService
       }
 
       Log.Logger = new LoggerConfiguration().ReadFrom
-          .Configuration(configuration)
-          .Enrich.WithProperty("Service", "NotificationService")
-          .WriteTo.Seq(
-              serverUrl: seqServerUrl,
-              apiKey: seqApiKey)
-          .CreateLogger();
+        .Configuration(configuration)
+        .Enrich.WithProperty("Service", "NotificationService")
+        .WriteTo.Seq(
+          serverUrl: seqServerUrl,
+          apiKey: seqApiKey)
+        .CreateLogger();
 
       try
       {
@@ -49,11 +49,11 @@ namespace LT.DigitalOffice.NotificationService
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-       Host.CreateDefaultBuilder(args)
-           .UseSerilog()
-           .ConfigureWebHostDefaults(webBuilder =>
-           {
-             webBuilder.UseStartup<Startup>();
-           });
+      Host.CreateDefaultBuilder(args)
+      .UseSerilog()
+      .ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
   }
 }
